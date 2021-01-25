6741f2c67c602273e926451af82ec46455db59b7
timalherbe <11437285+timalherbe@users.noreply.github.com>
2021-01-25 17:40:02 +0100

    Delete Terraform directory

--- Terraform/output.tf
+++
@@ -1,3 +0,0 @@
-output "instance_ip" {
-    value=aws_instance.server_timote.*.public_ip
-}
--- Terraform/vpc.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_default_vpc" "vpc_default_timote" {
-   tags = {
-     Name = "Default VPC Timote"
-   }
- }
